1d4851f23634e376346ff6fb98b6552c79772b10
ishikabansal394 <ishikabansal0224@gmail.com>
2026-02-16 10:00:00 +0530

    day 16 complete

---
+++ Ques31.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("enter size: ");
+    scanf("%d", &n);
+    int arr[100];
+    int freq[100];
+
+    printf("enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        freq[i] = -1;  
+    }
+
+    for(int i = 0; i < n; i++) {
+
+        if(freq[i] == 0)
+            continue;
+
+        int count = 1;
+
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+                freq[j] = 0;  
+            }
+        }
+
+        freq[i] = count;
+    }
+    for(int i = 0; i < n; i++) {
+        if(freq[i] != 0) {
+            printf("%d:%d ", arr[i], freq[i]);
+        }
+    }
+
+    return 0;
+}
